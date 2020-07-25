d7cc212741bbd339f2098e11d9db2d6005689c41
Lauren Stone <lstone321@gmail.com>
2020-07-25 16:51:04 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,11 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if "Hi, my name is Sophie.".end_with?(".")
+      return true 
+    else self 
+      return false 
+    end 
   end
 
   def question?
-
+    if "What's your name?".end_with?("?")
+      return true 
+    else 
+      return false 
+    end 
   end
 
   def exclamation?
